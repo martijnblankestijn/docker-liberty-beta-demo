9fa85f95395059c5509deae43e0515d9e92bdc68
Martijn Blankestijn <martijnblankestijn@gmail.com>
2015-03-22 13:25:28 +0100

    Added stopping and removing old containers Exposed the websphere liberty profile also to remote hosts Removed test-jarˆî

--- workshops/src/main/docker/run-db.sh
+++ workshops/src/main/docker/run-db.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+NAME=h2database
+CONTAINER=`docker ps --all --filter "name=$NAME" -q`
+
+# Stop and remove the old container
+docker stop $CONTAINER && docker rm $CONTAINER
+
 # -d     : keep the container running and return the prompt
 # --name : for linking to this container
 # zhil...: name of the image to use
--- workshops/src/main/docker/run-wlp.sh
+++ workshops/src/main/docker/run-wlp.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
-#docker build -t liberty-app .
+NAME=liberty-web
+IMAGE=liberty-docker-poc
+CONTAINER=`docker ps --all --filter "name=$NAME" -q`
+DB_CONTAINER=h2database
+
+# Stop and remove the old container (if a container was found)
+if [ "CONTAINER" ]; then docker stop $CONTAINER && docker rm $CONTAINER; fi
+
 # Linking, see https://docs.docker.com/userguide/dockerlinks/
 # Name of the images comes from the pom.xml
 #docker run -d -p 9080:9080 -p 9443:9443 --name liberty-web --link h2database:db liberty-docker-poc
-docker run -d -P --name liberty-web --link h2database:db liberty-docker-poc
+docker run -d -P --name ${NAME} --link ${DB_CONTAINER}:db $IMAGE
